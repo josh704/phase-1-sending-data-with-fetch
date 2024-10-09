5f976be66c4b95a5decad6addd8eae08f9b30771
josh704 <joshuangetich02@gmail.com>
2024-10-09 23:21:50 +0300

    fetch-data

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+    
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ name, email })
+    })
+    .then(function (response) {
+        return response.json();
+      })
+    .then(data => {
+        const idElement = document.createElement('p');
+        idElement.textContent = `New user ID: ${data.id}`;
+        document.body.appendChild(idElement);
+    })
+    .catch(error => {
+        const errorElement = document.createElement('p');
+        errorElement.textContent = `Error: ${error.message}`;
+        document.body.appendChild(errorElement);
+    });
+}
